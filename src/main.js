a2a42eed16054402186867febd1c0ec67f468282
VaynePeng <vaynepeng@gmail.com>
2022-11-19 03:01:10 +0800

    use loader

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import subtraction from './js/subtraction'
 import add from './js/add'
+import './css/index.css'
+import './css/index.less'
 
 console.log('subtraction', subtraction(5, 3, 1))
 console.log('add', add(5, 3, 1))
+
+import bg from './images/icon.png'
+const box1 = document.querySelector('.box1')
+box1.setAttribute('style', `background-image: url('${bg}')`)
--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,33 @@ module.exports = {
   },
   module: {
     // loader
-    rules: []
+    rules: [
+      {
+        test: /\.css$/i, // 正则匹配
+        // loader: 'css-loader', // loader 选项只能使用一个 loader 一般不建议使用，使用 use 就好
+        use: [
+          'style-loader', // 通过使用多个 style 标签自动把 styles 插入到 DOM 中
+          'css-loader' // 将 js 中的 css 通过使用多个 style 标签自动把 styles 插入到 DOM 中
+        ] // 匹配到的文件会使用这些 loader 从后往前加载
+      },
+      {
+        test: /\.less$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'less-loader' // 将 less 编译为 css
+        ]
+      },
+      {
+        test: /\.(png|jpg)$/i,
+        type: 'asset',
+        parser: {
+          dataUrlCondition: {
+            maxSize: 50 * 1024 // 小于 50kb 会转化为 base64
+          }
+        }
+      }
+    ]
   },
   plugins: []
 }
